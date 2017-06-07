2664cfc75505c09d8f1507974df6f3b3397d02dd
David Amanshia <phpengine@pharaohtools.com>
2017-06-07 10:20:54 +0100

    Index controller returns some data correctly

--- app/Index/Controller/IndexController.php
+++ app/Index/Controller/IndexController.php
@@ -5,8 +5,13 @@ Namespace Controller ;
 class IndexController extends \Controller\Base {
 
     public function execute($pageVars) {
+
         $page_model = new \Model\Index\PageModel() ;
         $res['page'] = $page_model->getPage() ;
+        $res['view'] = 'index.phptpl' ;
+        $res['type'] = 'view' ;
+        $res['view_control'] = 'Index' ;
+        \ISOPHP\js_core::$console->log('Ind Con', $res) ;
         return $res ;
     }
 
