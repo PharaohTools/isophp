651733dec246ae955bbf1398e4d49d520dfa3986
David Amanshia <phpengine@pharaohtools.com>
2017-06-07 03:41:22 +0100

    more copies

--- build/build_to_uniter.php
+++ build/build_to_uniter.php
@@ -22,7 +22,7 @@ foreach ($app_and_core as $app_or_core) {
     foreach ($cur_dir_array as $module_name) {
         $one_item = $cur_dir_path.DIRECTORY_SEPARATOR.$module_name ;
         if (is_file($one_item)) {
-            echo "\n\n Found File {$module_name}\n" ;
+            echo "\n Found File {$module_name}\n" ;
             $new_file = $client_directory . $module_name;
             if (substr($one_item, -4, 4) === '.php') {
                 echo "    Switching .php to .fephp\n";
@@ -48,11 +48,34 @@ foreach ($app_and_core as $app_or_core) {
                     copy($original_file, $new_file);
                 } else if (is_dir($item_path)) {
                     echo "  Found Directory {$item_path}\n";
-                    if (in_array($module_root_file_or_dir, array("Model", "View", "Controller"))) {
+                    if (in_array($module_root_file_or_dir, array("Model", "View", "Controller", "Base"))) {
                         echo "    Found PHP File Directory {$module_root_file_or_dir}, Converting\n";
                         $code_directory_contents = array_diff(scandir($item_path), array('.', '..'));
                         foreach ($code_directory_contents as $code_directory_file) {
-                            if ($module_root_file_or_dir == "View" && in_array($code_directory_file, $available_clients)) {
+                            if ($module_root_file_or_dir == "Base") {
+
+                                echo "    Found Core Base Directory " . $item_path . DIRECTORY_SEPARATOR . $code_directory_file . ", Copying\n";
+                                $core_base_directory_contents = array_diff(scandir($item_path . DIRECTORY_SEPARATOR . $code_directory_file), array('.', '..'));
+                                foreach ($core_base_directory_contents as $core_base_directory_file) {
+
+                                    $new_dir = $client_directory . $module_name . DIRECTORY_SEPARATOR . $module_root_file_or_dir . DIRECTORY_SEPARATOR . $code_directory_file;
+
+                                    if (!is_dir($new_dir)) {
+                                        echo "      Creating non existent directory {$new_dir}\n";
+                                        system('mkdir -p ' . $new_dir);
+                                    }
+
+                                    $temp_original_file = $item_path . DIRECTORY_SEPARATOR . $code_directory_file . DIRECTORY_SEPARATOR . $core_base_directory_file;
+                                    $new_file = $client_directory . $module_name . DIRECTORY_SEPARATOR . $module_root_file_or_dir . DIRECTORY_SEPARATOR . $code_directory_file . DIRECTORY_SEPARATOR . $core_base_directory_file;
+
+                                    if (substr($new_file, -4, 4) === '.php') {
+                                        echo "   Switching .php to .fephp\n";
+                                        $new_file = str_replace('.php', '.fephp', $new_file);
+                                    }
+                                    echo "Copying File {$temp_original_file} to {$new_file}\n";
+                                    copy($temp_original_file, $new_file);
+                                }
+                            } else if ($module_root_file_or_dir == "View" && in_array($code_directory_file, $available_clients)) {
 
                                 echo "    Found View Client Directory " . $item_path . DIRECTORY_SEPARATOR . $code_directory_file . ", Copying\n";
                                 $view_client_directory_contents = array_diff(scandir($item_path . DIRECTORY_SEPARATOR . $code_directory_file), array('.', '..'));
