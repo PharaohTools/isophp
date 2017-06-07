2d74d437342662ca6aeabc8d22e0b9008dc7d6a6
David Amanshia <phpengine@pharaohtools.com>
2017-06-07 10:24:05 +0100

    Executes through to view

--- core/Core/Router.php
+++ core/Core/Router.php
@@ -8,35 +8,41 @@ class Router {
     private	$route;
     private $availableRoutes = array() ;
 
+//    public function run($bootstrapParams) {
+//      \ISOPHP\js_core::$console->log('route is:', $this->route, "bs", $bootstrapParams) ;
+//      return $this->route ;
+//    }
+
     public function run($bootstrapParams) {
-      $this->bootstrapParams = $bootstrapParams;
-      $this->setCurrentRoute();
-      return $this->route ;
+        $this->bootstrapParams = $bootstrapParams;
+        $this->setCurrentRoute();
+        \ISOPHP\js_core::$console->log('route is:', $this->route) ;
+        return $this->route ;
     }
 
     private function setCurrentRoute() {
-      $this->getAvailableRoutes();
-      $defaultRoute = $this->getDefaultRoute();
-      $this->parseControllerAliases();
-      $this->setRouteController();
-      if ($this->route != $defaultRoute ) {
-        $this->setRouteAction();
-        $this->setRouteExtraParams(); }
+        $this->getAvailableRoutes();
+//        $defaultRoute = $this->getDefaultRoute();
+        $this->parseControllerAliases();
+        $this->setRouteController();
+//        if ($this->route != $defaultRoute ) {
+//            $this->setRouteAction();
+//            $this->setRouteExtraParams(); }
     }
 
     private function getAvailableRoutes() {
-      $allInfoObjects = AutoLoader::getInfoObjects() ;
-      $all_processed_arrays = array();
-      foreach ($allInfoObjects as $infoObject) {
-        $curKeys = array_keys($infoObject->routesAvailable());
-        $routesAvailable = $infoObject->routesAvailable();
-        foreach ($curKeys as $curKey) {
-          if (isset($all_processed_arrays[$curKey]) ) {
-            $curValues = $all_processed_arrays[$curKey];
-            $all_processed_arrays[$curKey] = array_merge($curValues, $routesAvailable[$curKey] ); }
-          else {
-            $all_processed_arrays[$curKey] = $routesAvailable[$curKey]; } } }
-      $this->availableRoutes = $all_processed_arrays;
+//      $allInfoObjects = \Core\AutoLoader::getInfoObjects() ;
+//      $all_processed_arrays = array();
+//      foreach ($allInfoObjects as $infoObject) {
+//        $curKeys = array_keys($infoObject->routesAvailable());
+//        $routesAvailable = $infoObject->routesAvailable();
+//        foreach ($curKeys as $curKey) {
+//          if (isset($all_processed_arrays[$curKey]) ) {
+//            $curValues = $all_processed_arrays[$curKey];
+//            $all_processed_arrays[$curKey] = array_merge($curValues, $routesAvailable[$curKey] ); }
+//          else {
+//            $all_processed_arrays[$curKey] = $routesAvailable[$curKey]; } } }
+      $this->availableRoutes = array("Index" => array("index"));
     }
 
     private function getDefaultRoute() {
@@ -45,36 +51,53 @@ class Router {
     }
 
     private function parseControllerAliases() {
-      $allInfoObjects = AutoLoader::getInfoObjects() ;
+//      $allInfoObjects = \Core\AutoLoader::getInfoObjects() ;
       $aliases = array();
-      foreach ($allInfoObjects as $infoObject) {
-        $aliases = array_merge( $aliases, $infoObject->routeAliases() ); }
+//      foreach ($allInfoObjects as $infoObject) {
+//        $aliases = array_merge( $aliases, $infoObject->routeAliases() ); }
       if (isset($this->bootstrapParams[1])) {
-        if (array_key_exists($this->bootstrapParams[1], $aliases)) {
+        if (\ISOPHP\core::$php->array_key_exists($this->bootstrapParams[1], $aliases)) {
           $this->bootstrapParams[1] = strtr($this->bootstrapParams[1], $aliases); } }
     }
 
     private function setRouteController() {
-        (isset($this->bootstrapParams[1]) && array_key_exists( $this->bootstrapParams[1], $this->availableRoutes ))
-            ? $this->route["control"] = $this->bootstrapParams[1] : $this->route = $this->getDefaultRoute();
+        if (isset($this->bootstrapParams[1])) {
+            if (\ISOPHP\core::$php->array_key_exists( $this->bootstrapParams[1], $this->availableRoutes )) {
+                $this->route["control"] = $this->bootstrapParams[1] ;
+            } else {
+                $this->route["control"] = $this->route = $this->getDefaultRoute();
+            }
+        }
+        else {
+            $this->route["control"] = $this->route = $this->getDefaultRoute();
+        }
+
     }
 
     private function setRouteAction() {
         $actionSet = isset($this->bootstrapParams[2]) ;
-        $correctAct = ($actionSet) ? in_array( $this->bootstrapParams[2], $this->availableRoutes[$this->bootstrapParams[1]] ) : false ;
-        ($actionSet && $correctAct) ? $this->route["action"] = $this->bootstrapParams[2] : $this->route = $this->getDefaultRoute();
+        if ($actionSet) {
+            $correctAct = \ISOPHP\core::$php->in_array( $this->bootstrapParams[2], $this->availableRoutes[$this->bootstrapParams[1]] ) ;
+        } else {
+            $correctAct = false ;
+        }
+        if ($actionSet && $correctAct) {
+            $this->route["action"] = $this->bootstrapParams[2] ;
+        } else {
+            $this->route["action"] = $this->route = $this->getDefaultRoute();
+        }
     }
 
     private function setRouteExtraParams() {
         $this->route["extraParams"] = array();
-        $numberOfExtraParams = count($this->bootstrapParams)-3;
+        $numberOfExtraParams = \ISOPHP\core::$php->count($this->bootstrapParams)-3;
         for ($i=3; $i<($numberOfExtraParams+3); $i++) {
             $this->route["extraParams"][] = $this->bootstrapParams[$i] ;}
     }
 
     private function setDefaultRouteExtraParams() {
         $this->route["extraParams"] = array();
-        $numberOfExtraParams = count($this->bootstrapParams)-1;
+        $numberOfExtraParams = \ISOPHP\core::$php->count($this->bootstrapParams)-1;
         for ($i=1; $i<($numberOfExtraParams+1); $i++) {
             $this->route["extraParams"][] = $this->bootstrapParams[$i] ;}
     }
--- core/Core/View.php
+++ core/Core/View.php
@@ -19,140 +19,32 @@ class View {
             else if ($vvLayoutCond2) { $viewVars["layout"] = "blank" ; }
             else { $viewVars["layout"] = "blank" ; } }
         $templateData = $this->loadTemplate ($view, $viewVars) ;
-        $data = $this->loadLayout ( $viewVars["layout"], $templateData, $viewVars) ;
-        $this->renderAll($data) ;
+        $this->renderAll($templateData) ;
     }
 
-    private function outFormOverrideParam($viewVars) {
-        if (isset($viewVars["route"])) {
-            for ($i = 0; $i<count($viewVars["route"]["extraParams"]); $i++) {
-                if (isset($viewVars["route"]["extraParams"][$i])) {
-                    $stp = strpos($viewVars["route"]["extraParams"][$i], '--output-format=') ;
-                    if (is_int($stp) && isset($viewVars["output-format"])) {
-                        $viewVars["params"]["output-format"] = $viewVars["output-format"] ;
-                        $viewVars["route"]["extraParams"][$i] = '--output-format='.$viewVars["output-format"] ; } } } }
-        return $viewVars ;
-    }
-
-  public function loadLayout ($layout, $templateData, Array $pageVars) {
-      ob_start();
-      $viewFileName = ucfirst($layout)."Layout.tpl.php";
-      if ($this->loadViewFile($viewFileName, $pageVars, $templateData) == true) {
-          return ob_get_clean(); }
-      else {
-          // @todo no! dont die
-          die ("View Layout Not Found\n"); }
-  }
-
-  public function loadTemplate ($view, Array $pageVars) {
-    ob_start();
-    $outputFormat = "" ;
-    if (isset($pageVars["params"]["output-format"])) {
-      $outputFormat = strtoupper($pageVars["params"]["output-format"]); }
-    if (isset($pageVars["params"]["output-format"])  && $pageVars["params"]["output-format"]=="AUTO") {
-      $outputFormat = strtoupper($pageVars["params"]["output-format"]); }
-    $viewFileName = ucfirst($view).$outputFormat."View.tpl.php";
-    if ($this->loadViewFile($viewFileName, $pageVars) == true) {
-        return ob_get_clean(); }
-    else if (substr($viewFileName, strlen($viewFileName)-16, 16) =="AUTOView.tpl.php" && $this->loadViewFile("DefaultAUTOView.tpl.php", $pageVars) == true) {
-        return ob_get_clean(); }
-    else {
-        // @todo no! dont die
-        die ("View Template $viewFileName for $outputFormat Not Found\n"); }
-  }
-
-  private function renderAll($processedData) {
-    echo $processedData;
-  }
-
-  private function renderMessages($pageVars) {
-    $outVar = '';
-    if (isset($pageVars["messages"])) {
-      foreach ($pageVars["messages"] as $message ) {
-        $outVar .= '***ERROR: '. $message."\n"; } }
-    $outVar .= "\n";
-    return $outVar;
-  }
-
-    private function renderLogs() {
-        $registry_values = new \Model\RegistryStore();
-        $logs = $registry_values::getValue("logs") ;
-        if (count($logs)>0) {
-            $lmsg = '
-            <div class="btn btn-info btn-sm pharaoh-message">
-                <a class="close" data-dismiss="alert">×</a> ';
-            foreach ($logs as $log) {
-                $lmsg .= "{$log}<br/>" ; }
-            $lmsg .= ' </div> ' ; }
+    public function loadTemplate ($view, Array $pageVars) {
+        $viewFileName = \ISOPHP\core::$php->ucfirst($view)  ;
+        $lvf = $this->loadViewFile($viewFileName, $pageVars) ;
+        \ISOPHP\js_core::$console->log('Template loaded', $lvf) ;
+        if ($lvf !== false) {
+            return $lvf; }
         else {
-            $lmsg = "" ; }
-        return $lmsg ;
+            // @todo no! dont die
+            die ("View Template $viewFileName Not Found\n"); }
     }
 
-    private function renderWidgetPosition() {
-        $registry_values = new \Model\RegistryStore();
-        $logs = $registry_values::getValue("logs") ;
-        if (count($logs)>0) {
-            $lmsg = '
-            <div class="btn btn-info btn-sm pharaoh-message">
-                <a class="close" data-dismiss="alert">×</a> ';
-            foreach ($logs as $log) {
-                $lmsg .= "{$log}<br/>" ; }
-            $lmsg .= ' </div> ' ; }
-        else {
-            $lmsg = "" ; }
-        return $lmsg ;
+    private function renderAll($processedData) {
+        echo $processedData;
     }
 
-  public function loadViewFile($viewFileName, $pageVars, $templateData=null) {
-    $allModuleParentDirectories = array("Extensions", "Modules", "Core");
-    foreach ($allModuleParentDirectories as $oneModuleParentDirectory) {
-        if ($oneModuleParentDirectory != "Core") {
-            $modulesParentDirFullPath = dirname(__FILE__).DIRECTORY_SEPARATOR . '..'  . DIRECTORY_SEPARATOR . $oneModuleParentDirectory ;
-            $modulesIndividualDirectories = scandir($modulesParentDirFullPath);
-            foreach ($modulesIndividualDirectories as $singleModuleDir) {
-              if (!in_array($singleModuleDir, array(".", ".."))) { // if not dot or double dot
-                  if ( is_dir($modulesParentDirFullPath.DIRECTORY_SEPARATOR.$singleModuleDir)) { // if is a dir
-                      $fileNameAndPath = $modulesParentDirFullPath . DIRECTORY_SEPARATOR . $singleModuleDir . DIRECTORY_SEPARATOR . 'Views' . DIRECTORY_SEPARATOR . $viewFileName;
-                      if (is_readable($fileNameAndPath)) {
-                          try {
-                              require $fileNameAndPath; }
-                          catch (\Exception $e) {
-                              // @todo logging with this
-                              \Core\BootStrap::setExitCode(1);
-                              return false ; }
-                          return true; } } } } }
-        else {
-            $modulesParentDirFullPath = dirname(__FILE__).DIRECTORY_SEPARATOR."Base".DIRECTORY_SEPARATOR."Views" ;
-            $modulesParentHelpDirFullPath = dirname(__FILE__).DIRECTORY_SEPARATOR."Help".DIRECTORY_SEPARATOR."Views" ;
-            $coreBaseViewFiles = scandir($modulesParentDirFullPath);
-            $coreHelpViewFiles = scandir($modulesParentHelpDirFullPath);
-            foreach ($coreBaseViewFiles as $coreViewFile) {
-                if (!in_array($coreViewFile, array(".", ".."))) { // if not dot or double dot
-                    $fileNameAndPath = $modulesParentDirFullPath . DIRECTORY_SEPARATOR . $coreViewFile;
-                    if (is_readable($fileNameAndPath) && $viewFileName == $coreViewFile) {
-                        try {
-                            require $fileNameAndPath; }
-                        catch (\Exception $e) {
-                            // @todo logging with this
-                            \Core\BootStrap::setExitCode(1);
-                            return false ; }
-                        return true; } } }
-            foreach ($coreHelpViewFiles as $coreViewFile) {
-                if (!in_array($coreViewFile, array(".", ".."))) { // if not dot or double dot
-                    $fileNameAndPath = $modulesParentHelpDirFullPath . DIRECTORY_SEPARATOR . $coreViewFile;
-                    if (is_readable($fileNameAndPath) && $viewFileName == $coreViewFile) {
-                        try {
-                            require $fileNameAndPath; }
-                        catch (\Exception $e) {
-                            // @todo logging with this
-                            \Core\BootStrap::setExitCode(1);
-                            return false ; }
-                        return true; } } }
-
-        }
+    public function loadViewFile($viewFileName, $pageVars, $templateData=null) {
+        \ISOPHP\js_core::$console->log('ViewFN', $viewFileName) ;
+        \ISOPHP\js_core::$console->log('View', $pageVars) ;
+        $data['one'] = "ab" ;
+        $data['two'] = "cd" ;
+        require_once ('/app/Index/View/web/Index.phptpl') ;
+        # instead of require, can we do an ajax load, then an eval, or js echo or eval?
+        return $data ;
     }
-    return false;
-  }
 
 }
