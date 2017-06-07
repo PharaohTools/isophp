54b71950a25b9e986ec8cc37ac2c2cf3f217ef9f
David Amanshia <phpengine@pharaohtools.com>
2017-06-07 03:27:46 +0100

    first controller draft

--- app/Index/Controller/IndexController.php
+++ app/Index/Controller/IndexController.php
@@ -1,10 +1,13 @@
 <?php
 
+Namespace Controller ;
+
 class IndexController extends \Controller\Base {
 
     public function execute($pageVars) {
-        $page = \Model\
-
+        $page_model = new \Model\IndexPageModel() ;
+        $res['page'] = $page_model->getPage() ;
+        return $res ;
     }
 
 }
