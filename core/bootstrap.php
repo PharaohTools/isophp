698139d7f50ea998143e521fa47ef2dbdff30e4e
David Amanshia <phpengine@pharaohtools.com>
2017-06-07 10:25:19 +0100

    Executes through to view

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -19,20 +19,26 @@ class BootStrap {
         $routeObject = new \Core\Router();
         $route = $routeObject->run($argv_or_boot_params_null);
         $emptyPageVars = array("messages"=>array(), "route"=>$route);
+        \ISOPHP\js_core::$console->log($route, $emptyPageVars) ;
         $this->executeControl($route["control"], $emptyPageVars);
     }
 
     public function executeControl($controlToExecute, $pageVars=null) {
+        \ISOPHP\js_core::$console->log('control class loading') ;
         $control = new \Core\Control();
+        \ISOPHP\js_core::$console->log('control class loaded', $control) ;
         $controlResult = $control->executeControl($controlToExecute, $pageVars);
+        \ISOPHP\js_core::$console->log('control res 1', $controlResult) ;
         if ($controlResult["type"]=="view") {
+            \ISOPHP\js_core::$console->log('control res 3', $controlResult) ;
             $this->executeView( $controlResult["view"], $controlResult["pageVars"] ); }
         else if ($controlResult["type"]=="control") {
+            \ISOPHP\js_core::$console->log('control res 2', $controlResult) ;
             $this->executeControl( $controlResult["control"], $controlResult["pageVars"] ); }
-        else {
-            $exception = new \Exception( 'No controller result type specified', 0);
-            $exception->run() ;
-        }
+//        else {
+//            $exception = new \Exception( 'No controller result type specified', 0);
+//            $exception->run() ;
+//        }
     }
 
     private function executeView($viewTemplate, $viewVars) {
