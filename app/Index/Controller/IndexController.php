328775b88b5d75dc4bb9ccdcf0c9f11ddbd21638
David Amanshia <phpengine@pharaohtools.com>
2017-06-07 03:40:53 +0100

    new model name

--- app/Index/Controller/IndexController.php
+++ app/Index/Controller/IndexController.php
@@ -5,7 +5,7 @@ Namespace Controller ;
 class IndexController extends \Controller\Base {
 
     public function execute($pageVars) {
-        $page_model = new \Model\IndexPageModel() ;
+        $page_model = new \Model\Index\PageModel() ;
         $res['page'] = $page_model->getPage() ;
         return $res ;
     }
