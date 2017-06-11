4ea8ea934094669f937ff0d6bd07e694f4cc2e95
David Amanshia <phpengine@pharaohtools.com>
2017-06-11 21:09:21 +0100

    Desktop application building

--- build/build-desktop-assets.dsl.php
+++ build/build-desktop-assets.dsl.php
@@ -9,22 +9,6 @@ RunCommand execute
   guess
 
 RunCommand execute
-  command "cd {{{ param::start-dir }}}/clients/desktop && ptdeploy vhe add -yg --vhe-url=$$subdomain.$$domain  --vhe-default-template-name=docroot-no-suffix"
+  label "Build the executable applications"
+  command "cd {{{ param::start-dir }}}/clients/desktop && electron-packager . ISOPHP --arch=ia32,x64 --out=exe --overwrite --platform=darwin,linux"
   guess
-
-RunCommand execute
-  label "Enable Site and restart Apache"
-  command "a2ensite {{{ var::subdomain }}}.$$domain.conf && service apache2 restart"
-  guess
-
-RunCommand execute
-  label "Start ISOPHP server "
-  command "bash -c 'php {{{ param::start-dir }}}/server/isophp_server.php &' > /var/log/isophp_server.log 2>&1 </dev/null"
-  nohup true
-  background
-  guess
-
-RunCommand execute
-  command "ptdeploy apachecontrol restart -yg"
-  guess
-electron-packager . ISOPHP --arch=ia32,x64 --out=exe --overwrite --platform=darwin,linux
