1b54516b97826bae8360c2505203f690fe2032a6
David Amanshia <phpengine@pharaohtools.com>
2017-06-11 19:17:18 +0100

    bootstrap works

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -6,7 +6,6 @@ class BootStrap {
 
     private static $exitCode ;
 
-
     public static function setExitCode($exitCode){
         self::$exitCode = $exitCode ;
     }
@@ -19,32 +18,26 @@ class BootStrap {
         $routeObject = new \Core\Router();
         $route = $routeObject->run($argv_or_boot_params_null);
         $emptyPageVars = array("messages"=>array(), "route"=>$route);
-        \ISOPHP\js_core::$console->log($route, $emptyPageVars) ;
+//        \ISOPHP\js_core::$console->log($route, $emptyPageVars) ;
         $this->executeControl($route["control"], $emptyPageVars);
     }
 
     public function executeControl($controlToExecute, $pageVars=null) {
-        \ISOPHP\js_core::$console->log('control class loading') ;
+//        \ISOPHP\js_core::$console->log('control class loading') ;
         $control = new \Core\Control();
-        \ISOPHP\js_core::$console->log('control class loaded', $control) ;
+//        \ISOPHP\js_core::$console->log('control class loaded', $control) ;
         $controlResult = $control->executeControl($controlToExecute, $pageVars);
-        \ISOPHP\js_core::$console->log('control res 1', $controlResult) ;
-        if ($controlResult["type"]=="view") {
-            \ISOPHP\js_core::$console->log('control res 3', $controlResult) ;
-            $this->executeView( $controlResult["view"], $controlResult["pageVars"] ); }
-        else if ($controlResult["type"]=="control") {
-            \ISOPHP\js_core::$console->log('control res 2', $controlResult) ;
-            $this->executeControl( $controlResult["control"], $controlResult["pageVars"] ); }
-//        else {
-//            $exception = new \Exception( 'No controller result type specified', 0);
-//            $exception->run() ;
-//        }
+//        \ISOPHP\js_core::$console->log('control res 1', $controlResult) ;
+        if ($controlResult->type == "view") {
+            $viewClass = '\View\\'.$controlResult->view_control.'View' ;
+//            \ISOPHP\js_core::$console->log('View Class Name Is:', $viewClass) ;
+            $view =  new $viewClass() ;
+            $view->executeView($controlResult->view_control, $controlResult->view, $controlResult->page);
+        }
+        else if ($controlResult->type == "control") {
+//            \ISOPHP\js_core::$console->log('control res 2', $controlResult) ;
+            $this->executeControl( $controlResult->control, $controlResult->page );
+        }
     }
-
-    private function executeView($viewTemplate, $viewVars) {
-        $view = new \Core\View();
-        $view->executeView($viewTemplate, $viewVars);
-    }
-
 
 }
