1bdfe34f1a7a89259080371bca4f5d21fe935743
David Amanshia <phpengine@pharaohtools.com>
2017-06-11 18:08:28 +0100

    More controller result properties

--- app/Index/Controller/IndexController.php
+++ app/Index/Controller/IndexController.php
@@ -7,10 +7,13 @@ class IndexController extends \Controller\Base {
     public function execute($pageVars) {
 
         $page_model = new \Model\Index\PageModel() ;
-        $res['page'] = $page_model->getPage() ;
-        $res['view'] = 'index.phptpl' ;
-        $res['type'] = 'view' ;
-        $res['view_control'] = 'Index' ;
+        $page = $page_model->getPage() ;
+        \ISOPHP\js_core::$console->log('ICP', $page) ;
+        $res = new \Controller\Result() ;
+        $res->page = $page ;
+        $res->view = 'index.phptpl' ;
+        $res->type = 'view' ;
+        $res->view_control = 'Index' ;
         \ISOPHP\js_core::$console->log('Ind Con', $res) ;
         return $res ;
     }
