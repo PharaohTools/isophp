ac1b45fb9328ca76d60345f525373d4b7b47c489
David Amanshia <phpengine@pharaohtools.com>
2017-06-14 18:20:31 +0100

    desktop loading electron class and autoloading

--- clients/desktop/app.js
+++ clients/desktop/app.js
@@ -7,24 +7,27 @@
 
 var $ = require('jquery'),
     php = require('phpjs'),
-    fileData = require('./uniter_bundle/fileData.js'),
-    mainFiles = require('./uniter_bundle/mainFiles.js'),
+    file_data = require('./uniter_bundle/file_data.js'),
+    file_index = require('./uniter_bundle/file_index.js'),
     hasOwn = {}.hasOwnProperty,
     uniter = require('uniter'),
     phpEngine = uniter.createEngine('PHP'),
     output = document.getElementById('output');
 
 var file_require_string = 'require("/core/constants.fephp") ; ' ;
+file_require_string += 'require("/core/electron.fephp") ; ' ;
 file_require_string += 'require("/core/isophp.fephp") ; ' ;
 file_require_string += 'require("/core/init.fephp") ; ' ;
 file_require_string += 'require("/core/bootstrap.fephp") ; ' ;
 file_require_string += 'require("/core/index.fephp") ; ' ;
 
 function getFileData(path) {
-    if (!hasOwn.call(fileData, path)) {
+    var pref_path = path.substr(1);
+    console.log('pref path ' . pref_path) ;
+    if (!file_data.hasOwnProperty(pref_path)) {
         throw new Error('Unknown file "' + path + '"');
     }
-    return fileData[path];
+    return file_data[pref_path];
 }
 
 // Set up a PHP module loader
@@ -45,6 +48,7 @@ phpEngine.expose(window.app, 'app');
 phpEngine.expose(window.BrowserWindow, 'BrowserWindow');
 phpEngine.expose(window.elec_path, 'path');
 phpEngine.expose(window.elec_url, 'url');
+phpEngine.expose(file_index, 'file_index');
 
 // Write content HTML to the DOM
 phpEngine.getStdout().on('data', function (data) {
