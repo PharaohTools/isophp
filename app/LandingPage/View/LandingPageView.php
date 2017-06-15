ce13dc1fd6bbc05a3be22f22606a8ca10149fbfd
David Amanshia <phpengine@pharaohtools.com>
2017-06-15 09:59:06 +0100

    Landing page module first draft

--- app/LandingPage/View/LandingPageView.php
+++ app/LandingPage/View/LandingPageView.php
@@ -2,12 +2,12 @@
 
 Namespace View ;
 
-class IndexView extends \Core\View {
+class LandingPageView extends \Core\View {
 
     public function display($data) {
 //        \ISOPHP\js_core::$console->log('data', $data ) ;
         $jQuery =  \ISOPHP\js_core::$jQuery ;
-        \ISOPHP\js_core::$window->document->title = 'Index' ;
+        \ISOPHP\js_core::$window->document->title = 'Landing Page' ;
         $tplfunc = \Core\View::$template ;
         $tpl_data = $tplfunc() ;
 //        \ISOPHP\js_core::$console->log('davey ravey don', $tpl_data) ;
