00083058c21d3448945bf40d6f3806fb7df89ebf
David Amanshia <phpengine@pharaohtools.com>
2017-06-15 09:58:46 +0100

    landing page controller

--- app/LandingPage/Controller/LandingPageController.php
+++ app/LandingPage/Controller/LandingPageController.php
@@ -1,7 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: pharaoh
- * Date: 06/06/17
- * Time: 15:16
- */
+
+Namespace Controller ;
+
+class LandingPageController extends \Controller\Base {
+
+    public function execute($pageVars) {
+        $page_model = new \Model\LandingPage\PageModel() ;
+        $page = $page_model->getPage() ;
+        \ISOPHP\js_core::$console->log('ICP', $page) ;
+        $res = new \Controller\Result() ;
+        $res->page = $page ;
+        $res->view = 'LandingPage.phptpl' ;
+        $res->type = 'view' ;
+        $res->view_control = 'LandingPage' ;
+        $res->post_template[] = $page_model->bindButtons() ;
+        \ISOPHP\js_core::$console->log('LandingPage Con', $res) ;
+        return $res ;
+    }
+
+}
