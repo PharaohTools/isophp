5214862bf4238d9ca20b466a69f25ec2bdabcb02
David Amanshia <phpengine@pharaohtools.com>
2017-06-15 21:49:02 +0100

    open dev tools again

--- clients/desktop/main.js
+++ clients/desktop/main.js
@@ -37,7 +37,7 @@ function createWindow () {
     }))
 
     // Open the DevTools.
-    // mainElectronWindow.webContents.openDevTools()
+    mainElectronWindow.webContents.openDevTools()
 
     // Emitted when the window is closed.
     mainElectronWindow.on('closed', function () {
