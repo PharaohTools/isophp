0aef1f911a35d3583c66b8e883eb50c000010343
David Amanshia <phpengine@pharaohtools.com>
2017-09-02 17:38:56 +0100

    vm vars set again

--- vars/vm.php
+++ vars/vm.php
@@ -1,9 +1,16 @@
 <?php
-include (__DIR__.DIRECTORY_SEPARATOR.'default.php') ;
+
+require_once(__DIR__.DIRECTORY_SEPARATOR.'default.php') ;
 $variables['vmname'] = $variables['application_slug'] ;
 $variables['domain'] = $variables['vmname'].'.vm' ;
 $variables['desktop_app_slug'] = $variables['vmname'] ;
 $variables['android_shell_script'] = 'vm-android-shell.bash' ;
-# Developer build can use a baack end of either
-$variables['mobilebackend'] = $params['mobilebackend'] ;
+# Developer build (Virtual Machine) can use a back end of either local (VM) or devcloud
+if (isset($params['mobilebackend'])) {
+    $variables['mobilebackend'] = $params['mobilebackend'] ;
+}
+else {
+    $variables['mobilebackend'] = 'local' ;
+}
+//var_dump('vm vars', __DIR__.DIRECTORY_SEPARATOR.'default.php', $variables) ;
 
