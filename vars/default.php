b111c5761411f0f53a3353b16d30b66a4c4d9c07
David Amanshia <phpengine@pharaohtools.com>
2017-09-02 17:42:28 +0100

    hide debug

--- vars/default.php
+++ vars/default.php
@@ -7,3 +7,5 @@ $variables['server_subdomain'] = 'server' ;
 $variables['domain'] = $variables['application_slug'].'.vm' ;
 $variables['friendly_app_slug'] = 'ISOPHP' ;
 $variables['desktop_app_slug'] = $variables['friendly_app_slug'] ;
+
+//var_dump('default vars', $variables) ;
