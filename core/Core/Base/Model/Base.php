a2d4858f6c07971d3e7de05e18588b35c95a3061
David Amanshia <phpengine@pharaohtools.com>
2017-09-02 05:04:06 +0100

    perform server request in base functionality

--- core/Core/Base/Model/Base.php
+++ core/Core/Base/Model/Base.php
@@ -19,5 +19,40 @@ class Base {
         return $str ;
     }
 
+    public function performRequest($request_vars, $afterwards) {
+
+        $jQuery = \ISOPHP\js_core::$jQuery ;
+        $new_data_string = '' ;
+
+        foreach ($request_vars as $one_var_key => $one_var_val) {
+            $new_data_string = $new_data_string.$one_var_key."=".$one_var_val.'&' ;
+        }
+        $length = \ISOPHP\core::$php->strlen($new_data_string) ;
+        $new_data_string = \ISOPHP\core::$php->substr($new_data_string, 0, $length-1) ;
+        $new_data_string = \ISOPHP\js_core::$window->encodeURI($new_data_string) ;
+
+        \ISOPHP\js_core::$console->log('perform request with: ', $new_data_string) ;
+        $data = array(
+            'type' => 'POST',
+            'url' => \Model\Configuration::$SERVER_URL,
+            'dataType'=> "json",
+            'data' => $new_data_string
+        ) ;
+
+        $ajax_object = $jQuery->ajax($data);
+        $status = $ajax_object->done(
+            function ($data) use ($jQuery, $afterwards) {
+//                // \ISOPHP\js_core::$console->log('returned request with 1: ', $data) ;
+                $this->returned_data = $data ;
+//                // \ISOPHP\js_core::$console->log('showing what afterwards is', $afterwards) ;
+                if ($afterwards !== null) {
+                    $afterwards($data) ;
+                }
+//                // \ISOPHP\js_core::$console->log('afterwards done') ;
+            }
+        );
+
+        return $status ;
+    }
 
 }
