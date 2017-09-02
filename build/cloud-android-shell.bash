c318b45cff0c0b0a4b6cb166d5492435c8175f5f
David Amanshia <phpengine@pharaohtools.com>
2017-09-02 05:20:22 +0100

    android shell for cloud environments

---
+++ build/cloud-android-shell.bash
@@ -0,0 +1,4 @@
+export PATH="$PATH:/opt/Android/Sdk:/opt/Android/Sdk/tools:/opt/Android/Sdk/tools/bin:/root/.gradle/wrapper/dists/gradle-3.3-all/55gk2rcmfc6p2dg9u9ohc3hw9/gradle-3.3/bin"
+export ANDROID_HOME="/opt/Android/Sdk"
+export ANDROID_SDK_ROOT="/opt/Android/Sdk"
+export GRADLE_HOME="/root/.gradle/wrapper/dists/gradle-3.3-all/55gk2rcmfc6p2dg9u9ohc3hw9/gradle-3.3/bin"
