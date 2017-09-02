c44c693e25b6928cf8d468f02fe642153275f1f2
David Amanshia <phpengine@pharaohtools.com>
2017-09-02 05:28:41 +0100

    android sdk bash for vm

---
+++ build/vm-android-shell.bash
@@ -0,0 +1,3 @@
+export PATH="$PATH:/home/ptbuild/Android/Sdk:/home/ptbuild/Android/Sdk/tools:/home/ptbuild/Android/Sdk/tools/bin:/home/ptbuild/.sdkman/candidates/gradle/current/bin"
+export ANDROID_HOME="/home/ptbuild/Android/Sdk"
+export ANDROID_SDK_ROOT="/home/ptbuild/Android/Sdk"
--- vars/default.php
+++ vars/default.php
@@ -4,5 +4,5 @@ $variables['application_slug'] = 'iso_php' ;
 $variables['subdomain'] = 'www' ;
 $variables['webclientsubdomain'] = 'www' ;
 $variables['server_subdomain'] = 'server' ;
-$variables['domain'] = $variables['application_slug'].'.local' ;
+$variables['domain'] = $variables['application_slug'].'.vm' ;
 $variables['desktop_app_slug'] = 'ISOPHP' ;
