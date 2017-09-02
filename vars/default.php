de8243862cab109acfcec3482b0c8ede93aff158
David Amanshia <phpengine@pharaohtools.com>
2017-09-02 05:10:33 +0100

    suitable default vars

--- vars/default.php
+++ vars/default.php
@@ -1,4 +1,8 @@
 <?php
 
+$variables['application_slug'] = 'iso_php' ;
 $variables['subdomain'] = 'www' ;
-$variables['domain'] = 'isophpframework.tld' ;
+$variables['webclientsubdomain'] = 'www' ;
+$variables['server_subdomain'] = 'server' ;
+$variables['domain'] = $variables['application_slug'].'.local' ;
+$variables['desktop_app_slug'] = 'ISOPHP' ;
