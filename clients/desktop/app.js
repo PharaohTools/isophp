03106791e9c316ccd184c142994cdfa33fcff7d4
David Amanshia <phpengine@pharaohtools.com>
2017-10-17 01:25:13 +0100

    desktop appjs updates

--- clients/desktop/app.js
+++ clients/desktop/app.js
@@ -18,14 +18,17 @@ var file_require_string = 'require("/core/constants.fephp") ; ' ;
 file_require_string += 'require("/core/app_vars.fephp") ; ' ;
 file_require_string += 'require("/core/electron.fephp") ; ' ;
 file_require_string += 'require("/core/isophp.fephp") ; ' ;
+file_require_string += '\\ISOPHP\\core::$php = $php ; ' ;
+file_require_string += '\\ISOPHP\\core::$file_index = $file_index ; ' ;
 file_require_string += 'require("/core/init.fephp") ; ' ;
 file_require_string += 'require("/core/WindowMessage.fephp") ; ' ;
 file_require_string += 'require("/core/bootstrap.fephp") ; ' ;
 file_require_string += 'require("/core/index.fephp") ; ' ;
 
+console.log('mainfiles app js', 'file data', file_data, 'file index', file_index) ;
+
 function getFileData(path) {
     var pref_path = path.substr(1);
-    console.log('pref path ' . pref_path) ;
     if (!file_data.hasOwnProperty(pref_path)) {
         throw new Error('Unknown file "' + path + '"');
     }
@@ -39,8 +42,12 @@ phpEngine.configure({
     }
 });
 
+console.log('engine configure app js') ;
+
 var this_console = console ;
 var this_window = window ;
+
+console.log('engine expose start app js') ;
 
 phpEngine.expose($, 'jQuery');
 phpEngine.expose(php, 'php');
@@ -52,6 +59,8 @@ phpEngine.expose(window.elec_path, 'path');
 phpEngine.expose(window.elec_url, 'url');
 phpEngine.expose(file_index, 'file_index');
 
+console.log('engine expose end app js') ;
+
 // Write content HTML to the DOM
 phpEngine.getStdout().on('data', function (data) {
     document.body.insertAdjacentHTML('beforeEnd', data);
@@ -60,7 +69,11 @@ phpEngine.getStdout().on('data', function (data) {
 // this is looking in the filedata file which is all the php compressed in a key value with path keys
 var php_code_string = '<?php '+file_require_string+' ?>' ;
 
+console.log('code string', php_code_string) ;
+
 // Go!
 phpEngine.execute(php_code_string).fail(function (error) {
     console.warn('ERROR: ' + error.toString());
 });
+
+console.log('End app js') ;
