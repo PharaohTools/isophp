fa1ab2f3ce9c9a650af54ac56f5a44ea05e433ca
David Amanshia <phpengine@pharaohtools.com>
2017-10-17 10:16:54 +0100

    default configuration files in each environment

---
+++ vars/configuration_local.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Model ;
+
+class Configuration {
+
+    public static $SERVER_URL ;
+
+    public function __construct() {
+        $variables = array() ;
+        require_once (__DIR__.DIRECTORY_SEPARATOR.'default.php') ;
+        self::$SERVER_URL = 'http://server.'.$variables['application_slug'].'.vm:8043' ;
+    }
+
+
+}
+
---
+++ vars/configuration_staging.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Model ;
+
+class Configuration {
+
+    public static $SERVER_URL ;
+
+    public function __construct() {
+        $variables = array() ;
+        require_once (__DIR__.DIRECTORY_SEPARATOR.'default.php') ;
+        self::$SERVER_URL = 'http://server.staging.'.$variables['domain'] ;
+    }
+
+}
+
