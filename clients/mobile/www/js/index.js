6f5dcfd50a42b2b1c696530a7eacfec6e3fbd3ef
David Amanshia <phpengine@pharaohtools.com>
2017-10-17 10:00:58 +0100

    xmlhttprequest to load the file

--- clients/mobile/www/js/index.js
+++ clients/mobile/www/js/index.js
@@ -1,3 +1,31 @@
+// progress on transfers from the server to the client (downloads)
+function updateProgress (oEvent) {
+    if (oEvent.lengthComputable) {
+        var percentComplete = oEvent.loaded / oEvent.total;
+        str = "The transfer is " + percentComplete + "% complete." ;
+        console.log(str);
+    } else {
+        // Unable to compute progress information since the total size is unknown
+        str = "Unable to compute progress information since the total size is unknown" ;
+        console.log(str);
+    }
+}
+
+function transferComplete(evt) {
+    console.log("The transfer is complete.");
+    console.log("Begin app code eval") ;
+    eval(this.responseText);
+    console.log("End app code eval") ;
+}
+
+function transferFailed(evt) {
+    console.log("An error occurred while transferring the file.");
+}
+
+function transferCanceled(evt) {
+    console.log("The transfer has been canceled by the user.");
+}
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -16,31 +44,29 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        // document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+function app_initialize() {
+    // document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+}
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        // this.receivedEvent('deviceready');
-        // document.getElementById("app-loader").className = "";
-        // alert('device ready notify') ;
-    },
+// deviceready Event Handler
+//
+// Bind any cordova events here. Common events are:
+// 'pause', 'resume', etc.
+function on_device_ready() {
+    // this.receivedEvent('deviceready');
+    // document.getElementById("app-loader").className = "";
+    console.log('device ready notify') ;
+    var oReq = new XMLHttpRequest();
+    console.log('new http req object') ;
+    oReq.addEventListener("progress", updateProgress);
+    oReq.addEventListener("load", transferComplete);
+    oReq.addEventListener("error", transferFailed);
+    oReq.addEventListener("abort", transferCanceled);
+    oReq.open("GET", "file:///android_asset/www/uniter_bundle/bundle.js");
+    oReq.send() ;
+    console.log('req open') ;
+}
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        // var parentElement = document.getElementById(id);
-        // var listeningElement = parentElement.querySelector('.listening');
-        // var receivedElement = parentElement.querySelector('.received');
-        // listeningElement.setAttribute('style', 'display:none;');
-        // receivedElement.setAttribute('style', 'display:block;');
-        // console.log('Received Event: ' + id);
-    }
-};
+app_initialize();
 
-app.initialize();
+document.addEventListener("deviceready", on_device_ready, false);
