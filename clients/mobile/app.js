b9ac9e5034c953de65d69245afe23fab7ee702b7
David Amanshia <phpengine@pharaohtools.com>
2017-12-04 23:01:07 +0000

    fixed fs and app for mobile

--- clients/mobile/app.js
+++ clients/mobile/app.js
@@ -7,61 +7,75 @@
 
 console.log('Start app js') ;
 
-var $ = require('jquery'),
-    php = require('phpjs'),
-    file_data = require('./www/uniter_bundle/file_data.js'),
-    file_index = require('./www/uniter_bundle/file_index.js'),
-    hasOwn = {}.hasOwnProperty,
-    uniter = require('uniter'),
-    phpEngine = uniter.createEngine('PHP'),
-    output = document.getElementById('output');
-
-// var file_require_string = "" ;
-// main_files.forEach(function (filePath) {
-//     file_require_string += 'require("'+filePath+'") ; ';
-// });
+var jq = require('jquery') ,
+    php = require('phpjs') ,
+    file_data = require('./www/uniter_bundle/file_data.js') ,
+    file_index = require('./www/uniter_bundle/file_index.js') ,
+    hasOwn = {}.hasOwnProperty ,
+    uniter = require('uniter') ,
+    phpEngine = uniter.createEngine('PHP') ;
 
 var file_require_string = 'require("/core/constants.fephp") ; ' ;
 file_require_string += 'require("/core/app_vars.fephp") ; ' ;
 file_require_string += 'require("/core/isophp.fephp") ; ' ;
+file_require_string += '\\ISOPHP\\cordova_core::$cordova = $cordova ; ' ;
+file_require_string += '\\ISOPHP\\cordova_core::$navigator = $navigator ; ' ;
+file_require_string += '\\ISOPHP\\core::$php = $php ; ' ;
 file_require_string += 'require("/core/init.fephp") ; ' ;
 file_require_string += 'require("/core/WindowMessage.fephp") ; ' ;
 file_require_string += 'require("/core/bootstrap.fephp") ; ' ;
 file_require_string += 'require("/core/index.fephp") ; ' ;
 
-console.log('mainfiles app js', 'file data', file_data, 'file index', file_index) ;
+console.log('mainfiles app js', 'file data', file_data) ;
+// console.log('mainfiles app js', 'file data', file_data) ;
 
 function getFileData(path) {
+
     var pref_path = path.substr(1);
-    console.log('pref path ' . pref_path) ;
-    if (!file_data.hasOwnProperty(pref_path)) {
-        throw new Error('Unknown file "' + path + '"');
+    console.log('gfd find path: ', pref_path) ;
+    if (pref_path === 'uniter_bundle/file_index.fephp') {
+        console.log('app js loading file index: ', file_index) ;
+        var file_string = "" ;
+        file_string += '<?php\n\n$file_index = array(\n' ;
+        file_index.forEach(function (one_short_path) {
+            file_string += '\t"' + one_short_path + '",\n' ;
+        }) ;
+        file_string += '\n) ;' ;
+        file_string += '\n\n\\ISOPHP\\core::$file_index = $file_index ;\n' ;
+        return file_string ;
+    } else {
+        if (!hasOwn.call(file_data, pref_path)) {
+            throw new Error('Unknown file "' + pref_path + '"');
+        }
+        // console.log('gfd return: ', file_data[pref_path]) ;
+        return file_data[pref_path];
     }
-    return file_data[pref_path];
+
 }
 
 // Set up a PHP module loader
 phpEngine.configure({
     include: function (path, promise) {
-        console.log('pref path ' . pref_path) ;
         promise.resolve(getFileData(path));
+        // promise.reject(console.log("Unable to locate file : " + path));
     }
 });
 
 console.log('engine configure app js') ;
 
-
 var this_console = console ;
 var this_window = window ;
 
 console.log('engine expose start app js') ;
+console.log('cordova vars', cordova, navigator) ;
 
-phpEngine.expose($, 'jQuery');
+phpEngine.expose(jq, 'jQuery');
 phpEngine.expose(php, 'php');
 phpEngine.expose(this_window, 'window');
 phpEngine.expose(this_console, 'console');
 phpEngine.expose(window.app, 'app');
-phpEngine.expose(file_index, 'file_index');
+phpEngine.expose(cordova, 'cordova');
+phpEngine.expose(navigator, 'navigator');
 
 console.log('engine expose end app js') ;
 
@@ -72,7 +86,6 @@ phpEngine.getStdout().on('data', function (data) {
 
 // this is looking in the filedata file which is all the php compressed in a key value with path keys
 var php_code_string = '<?php '+file_require_string+' ?>' ;
-
 console.log('code string', php_code_string) ;
 
 // Go!
@@ -88,5 +101,4 @@ console.log('find tags') ;
         console.error(error);
     });
 });
-
 console.log('End app js') ;
--- clients/mobile/fs.js
+++ clients/mobile/fs.js
@@ -40,6 +40,9 @@ files.forEach(function (filePath) {
     file_index.push(short_path) ;
 });
 
+// For mobile, need ot add the file index to the file data
+// file_data['uniter_bundle/file_index.fephp'] = '<?php\n\n$file_index = ' + JSON.stringify(file_index) + ' ;\n\n\\ISOPHP\\core::$file_index = $file_index ;' ;
+
 console.log("\n\nfile_data\n", file_data, JSON.stringify(file_data)) ;
 fs.writeFileSync(
     __dirname + '/www/uniter_bundle/file_data.js',
@@ -49,5 +52,23 @@ fs.writeFileSync(
 console.log("\n\nfile_index\n", JSON.stringify(file_index)) ;
 fs.writeFileSync(
     __dirname + '/www/uniter_bundle/file_index.js',
-    'module.exports = ' + JSON.stringify(file_index) + ';'
+    'module.exports = ' + JSON.stringify(file_index) + ' ;'
+);
+
+// File index
+
+console.log("\n\nfile_index\n", JSON.stringify(file_index)) ;
+
+var file_string ;
+file_string += '<?php\n\n$file_index = array(\n' ;
+
+file_index.forEach(function (one_short_path) {
+    file_string += '\t"' + one_short_path + '",\n' ;
+}) ;
+file_string += '\n) ;' ;
+file_string += '\n\n\\ISOPHP\\core::$file_index = $file_index ;\n' ;
+
+fs.writeFileSync(
+    __dirname + '/www/uniter_bundle/file_index.php',
+    file_string
 );
