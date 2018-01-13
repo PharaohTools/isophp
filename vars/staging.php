f26be2982a7387c16b56cb5c421ebdd5cc3243b3
David Amanshia <phpengine@pharaohtools.com>
2018-01-13 10:44:57 +0000

    default backend vars set

--- vars/staging.php
+++ vars/staging.php
@@ -3,3 +3,7 @@ include (__DIR__.DIRECTORY_SEPARATOR.'default.php') ;
 $variables['vmname'] = $variables['application_slug'] ;
 $variables['domain'] = $variables['vmname'].'.vm' ;
 $variables['desktop_app_slug'] = $variables['vmname'] ;
+
+
+$variables['backendenv'] = 'staging' ;
+$variables['custom_branch'] = $variables['backendenv'] ;
--- vars/vm.php
+++ vars/vm.php
@@ -5,12 +5,17 @@ $variables['vmname'] = $variables['application_slug'] ;
 $variables['domain'] = $variables['vmname'].'.vm' ;
 $variables['desktop_app_slug'] = $variables['vmname'] ;
 $variables['android_shell_script'] = 'vm-android-shell.bash' ;
+
+if (isset($params['backendenv'])) {
+    $variables['backendenv'] = $params['backendenv'] ;
+    if (in_array($params['backendenv'], array('local', 'devcloud'))) {
+        $variables['custom_branch'] = 'development' ;
+    }
+} else {
+    $params['backendenv'] = 'local' ;
+    $variables['backendenv'] = $params['backendenv'] ;
+}
+
 # Developer build (Virtual Machine) can use a back end of either local (VM) or devcloud
-if (isset($params['mobilebackend'])) {
-    $variables['mobilebackend'] = $params['mobilebackend'] ;
-}
-else {
-    $variables['mobilebackend'] = 'local' ;
-}
 //var_dump('vm vars', __DIR__.DIRECTORY_SEPARATOR.'default.php', $variables) ;
 
